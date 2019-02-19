578e56ff142335a128f911ff3c67ebddb48bb165
jordi96 <jordi96@JBSlaptop>
2019-02-20 00:55:54 +0100

    Segona proba de commit, comencem a omplir la classe Person

--- src/polimorfism/Person.java
+++ src/polimorfism/Person.java
@@ -1,5 +1,24 @@
 package polimorfism;
 
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Person {
+	
+	private List<Address> addresses = new ArrayList<Address>();
+	
+	public void printAddress() {
+		for(Address address: addresses) {
+			System.out.println(address);
+		}
+	}
+	
+	class Address{
+		private String country;
+		private String province;
+		private String street;
+		private int number;
+		
+	}
+	
 }
