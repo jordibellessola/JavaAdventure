6c272c4141c538ae37681f7d974abeb64a57f5ee
Jordi Bellés Solà <xodi96@hotmail.com>
2019-02-20 12:49:31 +0100

    Acabem implementacio Person i comencem TestPerson

--- src/polimorfism/Person.java
+++ src/polimorfism/Person.java
@@ -5,12 +5,31 @@ import java.util.List;
 
 public class Person {
 	
-	private List<Address> addresses = new ArrayList<Address>();
+	private List <Address> addresses = new ArrayList<Address>();
+	private int age;
+	private String name;
+	
+	public Person(String _name,int _age) {
+		this.name = _name;
+		this.age = _age;
+	}
+	
+	public void addAddress(Address _addresses) {
+		addresses.add(_addresses);
+	}
+	
+	public void addAddress(String _country,String _province,String _street,int _number) {
+		addresses.add(new Address(_country, _province, _street, _number ));
+	}
 	
 	public void printAddress() {
 		for(Address address: addresses) {
 			System.out.println(address);
 		}
+	}
+	
+	public String toString() {
+		return this.name+" "+this.age;
 	}
 	
 	class Address{
@@ -19,6 +38,17 @@ public class Person {
 		private String province;
 		private String street;
 		private int number;
+		
+		public Address(String _country,String _province,String _street,int _number){
+			this.country = _country;
+			this.province = _province;
+			this.street = _street;
+			this.number = _number;
+		}
+		
+		public String toString() {
+			return this.street+" "+this.number+" "+this.province+" "+this.country;
+		}
 		
 	}
 	
---
+++ src/polimorfism/TestPerson.java
@@ -0,0 +1,10 @@
+package polimorfism;
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		Person person = new Person("Jordi",23);
+		
+	}
+
+}
